65e7fc1272f174cbb24a7ff9cb6dd1342501c1be
Gary11777 <iharushakou@gmail.com>
2026-02-21 11:54:12 +0300

    E19 From Mixins to Composables

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,19 @@
 <script setup>
+import { useFlash } from '../composables/useFlash';
+
+let { flash } = useFlash();
 </script>
 
 <template>
-  <div>
-    <h1>About Page</h1>
-    <p>This is the about page.</p>
-  </div>
+  <main>
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+    <p>
+      <button @click="flash('Success', 'It Works On The About Page!', 'info')">Click me</button>
+    </p>
+  </main>
 </template>
 
 <style scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,27 @@
 <script setup>
+import { useFlash } from '../composables/useFlash';
 
-import { ref } from 'vue'
-
-let message = ref('Hello, Vue!')
-
-setTimeout(() => {
-  message.value = 'Hello, Vue! (updated)'
-}, 2000)
-
-const reverseMessage = () => {
-  message.value = message.value.split('').reverse().join('')
-}
-
+let { flash } = useFlash();
 </script>
 
 <template>
-  <div>
-    <h1>Home Page</h1>
-    <p>Welcome to the home page!</p>
-  </div>
-  <p>
+  
+  <!-- <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
   <p><b>{{ message }}</b></p>
   <p><input type="text" v-model="message" /></p>
-  <p><button @click="reverseMessage">Reverse Message</button></p>
+  <p><button @click="reverseMessage">Reverse Message</button></p> -->
+  <main>
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+    <p>
+      <button @click="flash('Test Title', 'It Works!')">Click me</button>
+    </p>
+  </main>
 </template>
 
 <style scoped>
